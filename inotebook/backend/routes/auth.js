5b9c06099a4bd3df10feda12a6aaeae0351453ba
Nyasha <nyashagupta8445@gmail.com>
2024-06-05 19:46:59 +0530

    token added used jwt(json web token) and bcryptjs

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router()
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const JWT_SECRET = 'Nyashaisawesome++'
+
 
 
 //create a user using post:endponit: POST "/api/auth/createuser". Doesn't require authentication
@@ -26,27 +32,36 @@ router.post('/createuser',
     if(user){
       return res.status(400).json({error:"Sorry a user with this email already exists"})
     }
-
+     
+    //generating and addin salt to password
+    const salt = await bcrypt.genSalt(10)
+    const secPass = await bcrypt.hash(req.body.password, salt)
     //new user creation
+   
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: secPass
     })
-    res.json(user)
+
+    const data = {
+      user:{
+        id:user.id
+      }
+    }
+
+    const authToken = jwt.sign(data, JWT_SECRET);
+    
+
+    // res.json(user)
+    res.json({authToken})
     
   }catch(error){
     console.error(error.message)
     res.status(500).send("Some error occured")
   }
   
-    //.then(user=>res.json(user)).catch(
-    //   err=>{
-    //     console.log(err)
-    //     res.join({error:"Please enter a unique value for email", message:err.message})
-    //   }
-    // );
-   })
+})
    
 
 
